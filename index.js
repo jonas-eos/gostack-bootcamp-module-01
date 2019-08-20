0450a4b452d28dc39a3938bc63eae31e5e245730
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-20 00:12:11 -0300

    feature: /users/:id param router
    Set a new router to /users that get user ID as a param
    and return Looking for user id: + user id on request array.

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ const express = require("express");
 const server = express();
 
 /**
- * Set a router to http://server:listen_port/teste
+ * Set a router to http://server:listen_port/users
  *
  * @param { Query } HTTP::__request
  * @param __response
  *
  * @return  json { query.name __response }
  */
-
 server.get("/users", (__request, __response) => {
   const name = __request.query.name;
   return __response.json({ message: `Hello ${name}` });
+});
+
+/**
+ * Set a router to http://server:listen_port/users/:id
+ *
+ * @param { Query } HTTP::__request
+ * @param __response
+ *
+ * @return  json { param.id __response }
+ */
+server.get("/users/:id", (__request, __response) => {
+  const { id } = __request.params;
+  return __response.json({ message: `Looking for user id:  ${id}` });
 });
 
 // Start a server on port 9090
