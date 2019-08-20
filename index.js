ca4c99dc292459df1cd7a0e6b91ec5813f72b9b2
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-20 00:20:14 -0300

    feature: users/index sample
    Sample code that get index as param and return an user from the users
    array.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 // Create a new object from express
 const server = express();
 
+// Users array sample
+const users = ["Jonas", "Diego", "Marcela"];
+
 /**
  * Set a router to http://server:listen_port/teste
  *
@@ -12,10 +15,22 @@ const server = express();
  *
  * @return  json { query.name __response }
  */
-
 server.get("/users", (__request, __response) => {
   const name = __request.query.name;
   return __response.json({ message: `Hello ${name}` });
+});
+
+/**
+ * Set a router to http://server:listen_port/users/:index
+ *
+ * @param { Param } HTTP::__request
+ * @param __response
+ *
+ * @return  json { user from a index param }
+ */
+server.get("/users/:index", (__request, __response) => {
+  const { index } = __request.params;
+  return __response.json(users[index]);
 });
 
 // Start a server on port 9090
